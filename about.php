32af06de9cc9c8c623f006cdbd2702654b2a4300
unknown <ramandadiio@gmail.com>
2024-07-02 14:57:21 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>About Us</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Google fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+
+    <style>
+
+.owl-carousel {
+  display: none;
+  position: relative;
+  width: 100%;
+  -ms-touch-action: pan-y;
+}
+.owl-carousel .owl-wrapper {
+  display: none;
+  position: relative;
+  -webkit-transform: translate3d(0px, 0px, 0px);
+}
+.owl-carousel .owl-wrapper-outer {
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+}
+.owl-carousel .owl-item img {
+  width: 45px;
+  padding: 7px !important;
+  background-color: #f6f5ff;
+  margin-left: 0px !important;
+  border-radius: 15px;
+}
+.owl-carousel .allitem {
+  padding: 25px;
+  border-radius: 0px;
+}
+.owl-carousel .owl-item {
+  float: left;
+  border-radius: 5px;
+  border: 1px solid rgba(87, 59, 255, 0.21);
+}
+.owl-controls .owl-page,
+.owl-controls .owl-buttons div {
+  cursor: pointer;
+}
+.owl-controls {
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 50);
+}
+
+/* mouse grab icon */
+.grabbing {
+  cursor: url(grabbing.png) 8 8, move;
+}
+
+/* fix */
+.owl-carousel .owl-wrapper,
+.owl-carousel .owl-item {
+  -webkit-backface-visibility: hidden;
+  -moz-backface-visibility: hidden;
+  -ms-backface-visibility: hidden;
+  -webkit-transform: translate3d(0, 0, 0);
+  -moz-transform: translate3d(0, 0, 0);
+  -ms-transform: translate3d(0, 0, 0);
+}
+
+.user-blog {
+  background: #0000;
+  padding: 0px 0px 0px;
+}
+
+div#owl-demo1 .item {
+  padding: 0 5px;
+}
+.blog-allof {
+  background: #fff;
+  border-radius: 10px;
+  margin-left: 30px !important;
+}
+
+.discretion-blog {
+  padding: 0px;
+  align-items: left;
+}
+
+.discretion-blog p {
+  font-size: 16px;
+  color: #595665;
+  padding-bottom: 15px !important;
+  margin-top: 0px;
+  line-height: 26px;
+  font-family: "DM Sans", Helvetica, Arial, Lucida, sans-serif;
+}
+
+.discretion-blog h3 {
+  color: #1f2d3d;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 0px;
+  margin-bottom: 10px;
+  line-height: 26px;
+  font-family: "DM Sans", Helvetica, Arial, Lucida, sans-serif;
+  margin-top: 25px !important;
+  margin-bottom: 20px !important;
+}
+
+.et_pb_module.et_pb_text_align_center {
+  text-align: left;
+}
+
+.user-blog .owl-theme .owl-controls .owl-page span {
+  width: 17px;
+  height: 17px;
+  background: #454343;
+  opacity: 1;
+}
+.user-blog .owl-theme .owl-controls .owl-page.active span,
+.user-blog .owl-theme .owl-controls.clickable .owl-page:hover span {
+  background: #82b53f;
+}
+
+
+    </style>
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+        <div class="container px1">
+            <a class="navbar-brand fw-bold" href="#page-top"><img src="assets/images/logo_hinsa.png" width="40px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Feature
+                <i class="bi-list"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto me-0 my-0 my-lg-0">
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="index.php">HOME</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="about.php">ABOUT US</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="#facilities">FACILITIES</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="product.php">OUR PRODUCT</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="gallery.php">GALLERY</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="#contactus">CONTACT US</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <!-- Mashead header-->
+    <header class="masthead">
+        <div class="row">sc
+            <div class="col-lg-12 px-5">
+                <!-- Mashead text and app badges-->
+                <div class="mb-5 mb-lg-0" style="text-align: center;">
+                    <span class="lh-1 mb-3" style="font-family: Montserrat; font-weight: bold; font-size: 70px; color:#373737;">About Us</span>
+                    <br>
+                    <br>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12 px-5">
+                <div class="mb-5 mb-lg-0" style="text-align: justify;">
+                    <span class="mb-5" style="font-family: Montserrat; font-size: 16px; color: #787878;">PT Hinsa Sejahtera Indonesia merupakan perusahaan supplier bahan baku yang diperuntukan Hotel, Catering, Restaurant, Supermarket dan instansi lainnya. Kami mulai berkembang membuat produk olahan frozen food seperti nugget ayam, brokoli frozen, mix vegetable frozen, jagung pipil frozen, capcay frozen dan fresh salad dengan kualitas terbaik dan penuh cita rasa..</span>
+                </div>
+            </div>
+        </div>
+
+
+    </header>
+    <!-- Quote/testimonial aside-->
+
+    <!-- App features section-->
+
+    <!-- Basic features section-->
+    <section style="background-color: #115174;   border-top-right-radius: 25px; border-top-left-radius: 25px;">
+        <div class="container" style="background-color: #115174;   border-top-right-radius: 25px; border-top-left-radius: 25px;">
+            <div class="row ">
+                <div class="col-md-12" style="text-align: center;">
+                    <h2 class="text-white lh-1 mb-4" style="font-family: Montserrat; font-size: 50px; font-weight:bold;">VISI</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12" style="text-align: justify;">
+                    <p style="font-family: Montserrat; font-size: 16px; color:white">Menjadi perusahaan dengan produk olahan yang terus berkembang dan menghasilkan produk-produk yang berkualitas, inovatif dan sehat untuk dikonsumsi oleh masyarakat.</p>
+                </div>
+            </div>
+            <br>
+            <br>
+            <div class="row ">
+                <div class="col-md-12" style="text-align: center;">
+                    <h2 class="text-white lh-1 mb-4" style="font-family: Montserrat; font-size: 50px; font-weight:bold;">MISI</h2>
+                </div>
+            </div>
+            <div class="row ">
+                <div class="col-md-12" style="text-align: justify;">
+                    <p style="font-family: Montserrat; font-size: 16px; color:white">Terus menambah kualitas dan semakin meningkatkan mutu dan standarisasi kualitas produk serta menambah jumlah supplier yang bisa menyediakan bahan baku yang baru dan inovatif dengan kualitas terbaik.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-auto" style="text-align: center;">
+                <img src="assets/images/sendok.png?123" width="100%" ;">
+                    <div style="width: 100%; overflow: hidden; justify-content: center; align-items: center; display:flex;">
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-6" style="align-items: center;">
+                <img src="assets/images/logo_hinsa.png" width="80px">
+            </div>
+            <div class="col-6">
+                <div class="row ">
+                    <div class="col ">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">HOME</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">ABOUT US</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">FACILITIES</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">OUR PRODUCT</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">GALLERY</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">CONTACT US</a> </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <!-- </section> -->
+    <!-- Footer-->
+    <footer class="text-center py-2" style="background: #115174;">
+        <div class="container px-5">
+            <div class="text-white-50 small">
+                <div class="mb-2">Copyright © 2024 IDGroup. All rights reserved.</div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- Feedback Modal-->
+    <div class="modal fade" id="feedbackModal" tabindex="-1" aria-labelledby="feedbackModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header bg-gradient-primary-to-secondary p-4">
+                    <h5 class="modal-title font-alt text-white" id="feedbackModalLabel">Send feedback</h5>
+                    <button class="btn-close btn-close-white" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body border-0 p-4">
+
+                    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                        <!-- Name input-->
+                        <div class="form-floating mb-3">
+                            <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
+                            <label for="name">Full name</label>
+                            <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                        </div>
+                        <!-- Email address input-->
+                        <div class="form-floating mb-3">
+                            <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
+                            <label for="email">Email address</label>
+                            <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                            <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                        </div>
+                        <!-- Phone number input-->
+                        <div class="form-floating mb-3">
+                            <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
+                            <label for="phone">Phone number</label>
+                            <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                        </div>
+                        <!-- Message input-->
+                        <div class="form-floating mb-3">
+                            <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
+                            <label for="message">Message</label>
+                            <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                        </div>
+                        <!-- Submit success message-->
+                        <!---->
+                        <!-- This is what your users will see when the form-->
+                        <!-- has successfully submitted-->
+                        <div class="d-none" id="submitSuccessMessage">
+                            <div class="text-center mb-3">
+                                <div class="fw-bolder">Form submission successful!</div>
+                                To activate this form, sign up at
+                                <br />
+                                <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                            </div>
+                        </div>
+                        <!-- Submit error message-->
+                        <!---->
+                        <!-- This is what your users will see when there is-->
+                        <!-- an error submitting the form-->
+                        <div class="d-none" id="submitErrorMessage">
+                            <div class="text-center text-danger mb-3">Error sending message!</div>
+                        </div>
+                        <!-- Submit Button-->
+                        <div class="d-grid"><button class="btn btn-primary rounded-pill btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+
+</html>
---
+++ product.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>About Us</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Google fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=DM+Sans:400,500,700&display=swap' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
+
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+
+    <style>
+        .newBody {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            /* height: 100vh; Ensure the body takes up the full height of the viewport */
+            font-family: "DM Sans", sans-serif;
+            transition: background 0.4s ease-in;
+        }
+
+        .newContainer {
+            width: 100%;
+            max-width: 100%;
+            /* Adjust the maximum width as needed */
+            transform-style: preserve-3d;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        input[type="radio"] {
+            display: none;
+        }
+
+        .cards {
+            position: relative;
+            width: 60%;
+            height: 200px;
+            /* Adjust height as needed */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .card {
+            position: absolute;
+            width: 100%;
+            /* Adjust the width of the cards */
+            height: 250px;
+            /* Adjust the height of the cards */
+            margin: auto;
+            transition: transform 0.4s ease;
+            cursor: pointer;
+            border-radius: 15px;
+        }
+
+        .ijg {
+            position: relative;
+        }
+
+        .ijg img {
+            width: 100%;
+            /* Adjust the width of the images */
+            height: 250px;
+            /* Adjust the height of the images */
+            border-radius: 15px;
+            object-fit: cover;
+        }
+
+        .image-text {
+            position: absolute;
+            bottom: 10px;
+            left: 10px;
+            color: white;
+            font-size: 20px;
+            font-family: 'Montserrat', sans-serif;
+            padding: 5px;
+            border-radius: 15px;
+        }
+
+        #item-1:checked~.cards #song-3,
+        #item-2:checked~.cards #song-1,
+        #item-3:checked~.cards #song-2 {
+            transform: translateX(-45%) scale(0.8);
+            opacity: 0.4;
+            z-index: 0;
+        }
+
+        #item-1:checked~.cards #song-2,
+        #item-2:checked~.cards #song-3,
+        #item-3:checked~.cards #song-1 {
+            transform: translateX(45%) scale(0.8);
+            opacity: 0.4;
+            z-index: 0;
+        }
+
+        #item-1:checked~.cards #song-1,
+        #item-2:checked~.cards #song-2,
+        #item-3:checked~.cards #song-3 {
+            transform: translateX(0) scale(1);
+            opacity: 1;
+            z-index: 1;
+        }
+    </style>
+</head>
+
+<body id="page-top">
+
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+        <div class="container px-1">
+            <a class="navbar-brand fw-bold" href="#page-top"><img src="assets/images/logo_hinsa.png" width="40px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Feature
+                <i class="bi-list"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto me-0 my-0 my-lg-0">
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="index.php">HOME</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="about.php">ABOUT US</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="#facilities">FACILITIES</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="product.php">OUR PRODUCT</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="gallery.php">GALLERY</a></li>
+                    <li class="nav-item" style="font-family:Montserrat; font-size: 15px; font-weight: bold"><a class="nav-link me-lg-0" href="#contactus">CONTACT US</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section>
+        <div class="container">
+            <div class="row py-5">
+                <div class="col" style="text-align: center; font-size:60px; font-family:Montserrat; font-weight:bolder">
+                    OUR PRODUCT
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="newBody">
+                        <div class="newContainer">
+                            <input type="radio" name="slider" id="item-1" checked>
+                            <input type="radio" name="slider" id="item-2">
+                            <input type="radio" name="slider" id="item-3">
+                            <div class="cards">
+                                <label class="card" for="item-1" id="song-1">
+                                    <span class="ijg"><img src="assets/images/banyak_buah.jpeg" alt="song">
+                                        <div class="image-text">Buah-buahan</div>
+                                    </span>
+                                </label>
+                                <label class="card" for="item-2" id="song-2">
+                                    <span class="ijg"><img src="assets/images/indian_condiments_with_copy_space_flat_lay_1.jpeg" alt="song">
+                                        <div class="image-text">Rempah-rempah</div>
+                                    </span>
+                                </label>
+                                <label class="card" for="item-3" id="song-3">
+                                    <span class="ijg"><img src="assets/images/front_view_vegetable_1.jpeg" alt="song">
+                                        <div class="image-text">Sayur-sayuran</div>
+                                    </span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-6" style="align-items: center;">
+                <img src="assets/images/logo_hinsa.png" width="80px">
+            </div>
+            <div class="col-6">
+                <div class="row ">
+                    <div class="col ">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">HOME</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">ABOUT US</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">FACILITIES</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">OUR PRODUCT</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">GALLERY</a> </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p style="text-align: right; font-family:Montserrat; font-size: 15px;"> <a href="#" style="text-decoration:none; color:black;">CONTACT US</a> </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <!-- </section> -->
+    <!-- Footer-->
+    <footer class="text-center py-2" style="background: #115174;">
+        <div class="container px-5">
+            <div class="text-white-50 small">
+                <div class="mb-2">Copyright © 2024 IDGroup. All rights reserved.</div>
+
+            </div>
+        </div>
+    </footer>
+
+    <script>
+        $('input').on('change', function() {
+            $('body').toggleClass('blue');
+        });
+    </script>
+     <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    
+
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+
+</body>
+
+</html>
